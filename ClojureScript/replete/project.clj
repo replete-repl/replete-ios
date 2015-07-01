5597810624d6a7531f882124ebb73ce6bde9f670
Mike Fikes <mike@fikesfarm.com>
2015-06-30 22:16:29 -0400

    Update to latest ClojureScript master

--- ClojureScript/replete/project.clj
+++ ClojureScript/replete/project.clj
@@ -1,4 +1,4 @@
 (defproject replete "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
-                 [org.clojure/clojurescript "0.0-3411"]
+                 [org.clojure/clojurescript "0.0-3422"]
                  [org.clojure/tools.reader "0.10.0-SNAPSHOT" :exclusions [org.clojure/clojure]]])
