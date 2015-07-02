a3f8c32e8d69719ae5d8298a428aec1650781828
Mike Fikes <mike@fikesfarm.com>
2015-07-02 13:19:32 -0400

    Update to released version of Clojure

--- ClojureScript/replete/project.clj
+++ ClojureScript/replete/project.clj
@@ -1,4 +1,4 @@
 (defproject replete "0.1.0"
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3422"]
                  [org.clojure/tools.reader "0.10.0-SNAPSHOT" :exclusions [org.clojure/clojure]]])
