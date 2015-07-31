1a95ab44214901eba8fe1f4e5c3bab6d13fe42be
asheldo <andy.m.sheldon@gmail.com>
2015-07-31 08:05:31 -0400

    Update cljs dependency
    1.7.10 has arrived

--- ClojureScript/replete/project.clj
+++ ClojureScript/replete/project.clj
@@ -1,5 +1,5 @@
 (defproject replete "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3459"]
+                 [org.clojure/clojurescript "1.7.10"]
                  [org.clojure/tools.reader "0.10.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
   :clean-targets ["out" "target"])
