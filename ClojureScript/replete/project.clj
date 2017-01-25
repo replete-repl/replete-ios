a5853eccd6f10c7e84a2b7412e6bcc88dd324fa5
Mike Fikes <mike@fikesfarm.com>
2017-01-24 22:35:10 -0500

    Update references to bundled libs

--- ClojureScript/replete/project.clj
+++ ClojureScript/replete/project.clj
@@ -1,5 +1,5 @@
 (defproject replete "0.1.0"
-  :dependencies [[andare "0.4.0"]
+  :dependencies [[andare "0.4.0"]                           ; Update in script/build also
                  [cljsjs/parinfer "1.8.1-0"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]
@@ -8,7 +8,7 @@
                  [malabarba/lazy-map "1.1"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
-                 [org.clojure/test.check "0.9.1-SNAPSHOT"]] ; Clone and build test.check master
+                 [org.clojure/test.check "0.9.1-SNAPSHOT"]] ; Clone and build test.check master (ref'd in script/build)
   :clean-targets ["out" "target"]
   :plugins [[lein-cljsbuild "1.1.5"]]
   :cljsbuild {:builds {:test {:source-paths ["src" "test"]
