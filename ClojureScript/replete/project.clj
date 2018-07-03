58fa7fac25e71c2debaac60a8bccf0d0355445f5
Mike Fikes <mike@fikesfarm.com>
2018-07-02 21:33:45 -0400

    Bump version

--- ClojureScript/replete/project.clj
+++ ClojureScript/replete/project.clj
@@ -1,13 +1,13 @@
 (defproject replete "0.1.0"
   :dependencies [[andare "0.9.0"]                           ; Update in script/build also
                  [cljsjs/parinfer "1.8.1-0"]
-                 [com.cognitect/transit-clj "0.8.300"]
-                 [com.cognitect/transit-cljs "0.8.243"]
+                 [com.cognitect/transit-clj "0.8.309"]
+                 [com.cognitect/transit-cljs "0.8.248"]
                  [fipp "0.6.8"]
                  [tailrecursion/cljson "1.0.7"]
                  [malabarba/lazy-map "1.3"]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.329"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/test.check "0.10.0-alpha2"]] 
   :clean-targets ["out" "target"]
   :plugins [[lein-cljsbuild "1.1.7"]]
